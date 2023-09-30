c593697a4fea7946b81b331f18f723fac7d185dd
Ian McClue <ian@nocta.ai>
2023-09-30 19:08:32 +0100

    Changes: site.config.ts made changes to the title

--- src/site.config.ts
+++ src/site.config.ts
@@ -4,7 +4,7 @@ export const siteConfig: SiteConfig = {
 	// Used as both a meta property (src/components/BaseHead.astro L:31 + L:49) & the generated satori png (src/pages/og-image/[slug].png.ts)
 	author: "Ian",
 	// Meta property used to construct the meta title property, found in src/components/BaseHead.astro L:11
-	title: "Astro Theme Cactus",
+	title: "FR99 Blog Site",
 	// Meta property used as the default description meta property
 	description: "FR99 Blog",
 	// HTML lang property, found in src/layouts/Base.astro L:18
